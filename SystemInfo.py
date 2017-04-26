df376ffccf36acfa74d6210cdf145d97e6e9899b
Mithilesh Kunal <mithileshkunal@gmail.com>
2017-04-26 23:01:49 +0530

    Added SystemInfo file

---
+++ SystemInfo.py
@@ -0,0 +1,6 @@
+# This program is to display the current version of Python.
+# Author : Mithilesh Kunal Dornadula S S
+# Date : 26-04-2017 22:50
+import sys
+print(sys.version)
+print(sys.version_info)
