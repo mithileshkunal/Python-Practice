1e2874f438b1c9cfa40c20153489695a4691e551
Mithilesh Kunal Dornadula S S <mithilesh.dornadula@cotiviti.com>
2019-02-25 15:30:34 +0530

    Add logging_every_iteration file

---
+++ logging_every_iteration.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+
+
+def my_custom_logger(logger_name, level=logging.DEBUG):
+    """
+    Method to return a custom logger with the given name and level
+    """
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(level)
+    format_string = ("%(asctime)s — %(name)s — %(levelname)s — %(funcName)s:"
+                     "%(lineno)d — %(message)s")
+    log_format = logging.Formatter(format_string)
+    # Creating and adding the console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(log_format)
+    logger.addHandler(console_handler)
+    # Creating and adding the file handler
+    file_handler = logging.FileHandler(logger_name, mode='a')
+    file_handler.setFormatter(log_format)
+    logger.addHandler(file_handler)
+    return logger
+
+
+if __name__ == "__main__":
+    for item in range(10):
+        logger = my_custom_logger(f"Logger{item}")
+        logger.debug(item)
