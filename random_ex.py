62e0e4151dae333f910d2e02a7ff7f26252c4653
Mithilesh Kunal Dornadula S S <mithilesh.dornadula@cotiviti.com>
2019-08-15 14:39:44 +0530

    Update random_ex

--- random_ex.py
+++ random_ex.py
@@ -1,9 +1,15 @@
 from utils import my_logging
 import random
 
-my_log = my_logging.my_console_logger(__name__, 20)
-a = [x for x in range(10)]
-my_log.info("Before shuffling:{}".format(a))
-random.shuffle(a)
-my_log.info("After shuffling:{}".format(a))
-my_log.info(random.random())
+
+def random_demo():
+    my_log = my_logging.my_console_logger(__name__, 20)
+    a = [x for x in range(10)]
+    my_log.info("Before shuffling:{}".format(a))
+    random.shuffle(a)
+    my_log.info("After shuffling:{}".format(a))
+    my_log.info(random.random())
+
+
+if __name__ == "__main__":
+    random_demo()
